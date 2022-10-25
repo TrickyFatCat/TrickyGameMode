d7156eb5e45cd297067d2890072ba045a38c43f9
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-25 20:57:43 +0300

    Implement a preparation timer

--- Source/TrickyGameMode/Private/GameModeSession.cpp
+++ Source/TrickyGameMode/Private/GameModeSession.cpp
@@ -11,12 +11,19 @@ void AGameModeSession::StartPlay()
 {
 	Super::StartPlay();
 
-	
+	if (PreparationDuration <= 0.f)
+	{
+		StartSession();
+	}
+	else if (!IsTimerActive(PreparationTimer))
+	{
+		GetWorldTimerManager().SetTimer(PreparationTimer, this, &AGameModeSession::StartSession, PreparationDuration);
+	}
 }
 
 bool AGameModeSession::SetPause(APlayerController* PC, FCanUnpause CanUnpauseDelegate)
 {
-	if (CurrentState != EGameModeState::GameOver)
+	if (CurrentState != EGameModeState::Finished)
 	{
 		SetState(EGameModeState::Pause);
 	}
@@ -24,15 +31,41 @@ bool AGameModeSession::SetPause(APlayerController* PC, FCanUnpause CanUnpauseDel
 	{
 		return false;
 	}
-	
+
 	return Super::SetPause(PC, CanUnpauseDelegate);
 }
 
 bool AGameModeSession::ClearPause()
 {
 	SetState(PreviousState);
-	
+
 	return Super::ClearPause();
+}
+
+float AGameModeSession::GetPreparationDuration() const
+{
+	return PreparationDuration;
+}
+
+void AGameModeSession::SetPreparationDuration(const float Value)
+{
+	PreparationDuration = Value < 0.f ? 0.f : Value;
+}
+
+void AGameModeSession::StartSession()
+{
+	if (!GetWorld())
+	{
+		return;
+	}
+
+	SetState(EGameModeState::InProgress);
+}
+
+void AGameModeSession::FinishSession(const bool bIsVictory)
+{
+	SetState(EGameModeState::Finished);
+	OnSessionFinished.Broadcast(bIsVictory);
 }
 
 void AGameModeSession::SetState(const EGameModeState& NewState)
@@ -45,4 +78,16 @@ void AGameModeSession::SetState(const EGameModeState& NewState)
 	PreviousState = CurrentState;
 	CurrentState = NewState;
 	OnStateChanged.Broadcast(CurrentState);
+}
+
+bool AGameModeSession::IsTimerActive(const FTimerHandle& TimerHandle) const
+{
+	if (!GetWorld())
+	{
+		return false;
+	}
+
+	const FTimerManager& TimerManager = GetWorldTimerManager();
+
+	return TimerManager.IsTimerActive(TimerHandle);
 }
--- Source/TrickyGameMode/Public/GameModeSession.h
+++ Source/TrickyGameMode/Public/GameModeSession.h
@@ -11,12 +11,14 @@ enum class EGameModeState : uint8
 {
 	Inactive,
 	Preparation,
-	Active,
+	InProgress,
 	Pause,
-	GameOver
+	Finished
 };
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnStateChangedSignature, EGameModeState, NewState);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnSessionFinishedSignature, bool, bIsVictory);
 
 /**
  * 
@@ -26,25 +28,52 @@ class TRICKYGAMEMODE_API AGameModeSession : public AGameModeBase
 {
 public:
 	GENERATED_BODY()
-	
+
 	AGameModeSession();
 
 	UPROPERTY(BlueprintAssignable, Category="GameMode")
 	FOnStateChangedSignature OnStateChanged;
 
+	UPROPERTY(BlueprintAssignable, Category="GameMode")
+	FOnSessionFinishedSignature OnSessionFinished;
+	
 	virtual void StartPlay() override;
 
 	virtual bool SetPause(APlayerController* PC, FCanUnpause CanUnpauseDelegate = FCanUnpause()) override;
 
 	virtual bool ClearPause() override;
+
+	UFUNCTION(BlueprintGetter, Category="TrickyGameMode")
+	float GetPreparationDuration() const;
+
+	UFUNCTION(BlueprintSetter, Category="TrickyGameMode")
+	void SetPreparationDuration(const float Value);
 	
+	void StartSession();
+
+	void FinishSession(const bool bIsVictory);
+
 private:
 	// States
-
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="GameMode", meta=(AllowPrivateAccess))
 	EGameModeState CurrentState = EGameModeState::Inactive;
 
 	EGameModeState PreviousState = EGameModeState::Inactive;
 
 	void SetState(const EGameModeState& NewState);
+
+	// Preparation timer
+	UPROPERTY(EditAnywhere,
+		BlueprintGetter=GetPreparationDuration,
+		BlueprintSetter=SetPreparationDuration,
+		Category="GameMode",
+		meta=(AllowPrivateAccess, ClampMin="0"))
+	float PreparationDuration = 3.f;
+
+	UPROPERTY(BlueprintReadOnly, Category="GameMode", meta=(AllowPrivateAccess))
+	FTimerHandle PreparationTimer;
+
+	bool IsTimerActive(const FTimerHandle& TimerHandle) const;
+
+
 };
