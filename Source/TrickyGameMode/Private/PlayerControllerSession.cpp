a6f530bdf434125482ecb768b9f771b55cd6e737
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-26 22:00:07 +0300

    Create player controller

---
+++ Source/TrickyGameMode/Private/PlayerControllerSession.cpp
@@ -0,0 +1,83 @@
+// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+
+#include "PlayerControllerSession.h"
+
+#include "Kismet/GameplayStatics.h"
+
+APlayerControllerSession::APlayerControllerSession()
+{
+}
+
+void APlayerControllerSession::BeginPlay()
+{
+	Super::BeginPlay();
+
+	const UWorld* World = GetWorld();
+
+	if (World)
+	{
+		AGameModeSession* GameMode = Cast<AGameModeSession>(World->GetAuthGameMode());
+
+		if (GameMode)
+		{
+			GameMode->OnStateChanged.AddDynamic(this, &APlayerControllerSession::OnGameModeStateChanged);
+		}
+	}
+
+	bShowMouseCursor = false;
+	SetTickableWhenPaused(true);
+}
+
+void APlayerControllerSession::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	FInputActionBinding& PauseBinding = InputComponent->BindAction("Pause",
+	                                                               IE_Pressed,
+	                                                               this,
+	                                                               &APlayerControllerSession::PauseGame);
+	PauseBinding.bExecuteWhenPaused = true;
+}
+
+void APlayerControllerSession::OnGameModeStateChanged(EGameModeState NewState)
+{
+	auto ToggleInput = [&](const bool bEnableInput, const bool bShowCursor, const FInputModeDataBase& InputMode)
+	{
+		bEnableInput ? EnableInput(this) : DisableInput(this);
+		bShowMouseCursor = bShowCursor;
+		SetInputMode(InputMode);
+	};
+
+	switch (NewState)
+	{
+	case EGameModeState::InProgress:
+		ToggleInput(true, bShowCursorOnStart, FInputModeGameOnly());
+		break;
+
+	case EGameModeState::Finished:
+		ToggleInput(false, true, FInputModeUIOnly());
+
+		if (!GetPawn())
+			break;
+
+		StopMovement();
+		GetPawn()->TurnOff();
+		break;
+
+	case EGameModeState::Pause:
+		ToggleInput(true, true, FInputModeGameAndUI());
+		break;
+
+	default:
+		ToggleInput(false, true, FInputModeUIOnly());
+		break;
+	}
+}
+
+void APlayerControllerSession::PauseGame()
+{
+	IsPaused()
+		? UGameplayStatics::SetGamePaused(GetWorld(), false)
+		: UGameplayStatics::SetGamePaused(GetWorld(), true);
+}
---
+++ Source/TrickyGameMode/Public/PlayerControllerSession.h
@@ -0,0 +1,34 @@
+// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GameModeSession.h"
+#include "PlayerControllerSession.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRICKYGAMEMODE_API APlayerControllerSession : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	APlayerControllerSession();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void SetupInputComponent() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Controller")
+	bool bShowCursorOnStart = false;
+	
+	UFUNCTION()
+	void OnGameModeStateChanged(EGameModeState NewState);
+
+	UFUNCTION()
+	void PauseGame();
+};
