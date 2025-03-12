f5296ea72b0b07222d59a632d96c5f523f8511ba
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-12 20:53:55 +0200

    Create game mode base class

---
+++ Source/TrickyGameMode/Private/TrickyGameModeBase.cpp
@@ -0,0 +1,76 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "TrickyGameModeBase.h"
+
+void ATrickyGameModeBase::SetInitialState(const EGameState Value)
+{
+	if (InitialState == Value)
+	{
+		return;
+	}
+
+	InitialState = Value;
+	CurrentState = InitialState;
+}
+
+bool ATrickyGameModeBase::StartGame_Implementation()
+{
+	if (CurrentState != EGameState::Inactive)
+	{
+		return false;
+	}
+
+	CurrentState = EGameState::Active;
+	OnGameStarted.Broadcast();
+	return true;
+}
+
+bool ATrickyGameModeBase::FinishGame_Implementation(const EGameResult Result)
+{
+	if (CurrentState != EGameState::Active)
+	{
+		return false;
+	}
+
+	CurrentState = EGameState::Finished;
+	OnGameFinished.Broadcast(Result);
+	return true;
+}
+
+bool ATrickyGameModeBase::StopGame_Implementation(const EGameInactivityReason Reason)
+{
+	if (CurrentState != EGameState::Active)
+	{
+		return false;
+	}
+
+	CurrentState = EGameState::Inactive;
+	OnGameStopped.Broadcast(Reason);
+	return true;
+}
+
+bool ATrickyGameModeBase::PauseGame_Implementation()
+{
+	if (CurrentState == EGameState::Paused)
+	{
+		return false;
+	}
+
+	LastState = CurrentState;
+	CurrentState = EGameState::Paused;
+	OnGamePaused.Broadcast();
+	return true;
+}
+
+bool ATrickyGameModeBase::UnpauseGame_Implementation()
+{
+	if (CurrentState != EGameState::Paused)
+	{
+		return false;
+	}
+	
+	CurrentState = LastState;
+	OnGameUnpaused.Broadcast();
+	return true;
+}
---
+++ Source/TrickyGameMode/Public/TrickyGameModeBase.h
@@ -0,0 +1,65 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameStateControllerInterface.h"
+#include "GameFramework/GameModeBase.h"
+#include "TrickyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRICKYGAMEMODE_API ATrickyGameModeBase : public AGameModeBase, public IGameStateControllerInterface
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnGameStartedDynamicSignature OnGameStarted;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnGameFinishedDynamicSignature OnGameFinished;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnGamePausedDynamicSignature OnGamePaused;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnGameUnpausedDynamicSignature OnGameUnpaused;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnGameStoppedDynamicSignature OnGameStopped;
+
+	UFUNCTION(BlueprintGetter, Category=GameState)
+	FORCEINLINE EGameState GetInitialState() const { return InitialState; };
+
+	UFUNCTION(BlueprintSetter, Category=GameState)
+	void SetInitialState(const EGameState Value);
+
+	UFUNCTION(BlueprintCallable, Category=GameState)
+	FORCEINLINE EGameState GetCurrentState() const { return CurrentState; };
+
+	UFUNCTION(BlueprintCallable, Category=GameState)
+	FORCEINLINE EGameState GetLastState() const { return LastState; };
+
+	virtual bool StartGame_Implementation() override;
+
+	virtual bool FinishGame_Implementation(const EGameResult Result) override;
+
+	virtual bool StopGame_Implementation(const EGameInactivityReason Reason) override;
+
+private:
+	UPROPERTY(EditInstanceOnly, BlueprintGetter=GetInitialState, BlueprintSetter=SetInitialState, Category=GameState)
+	EGameState InitialState = EGameState::Inactive;
+
+	UPROPERTY(VisibleAnywhere, BlueprintGetter=GetCurrentState, Category=GameState)
+	EGameState CurrentState = EGameState::Inactive;
+
+	UPROPERTY(VisibleAnywhere, BlueprintGetter=GetLastState, Category=GameState)
+	EGameState LastState = EGameState::Inactive;
+	
+	virtual bool PauseGame_Implementation() override;
+	
+	virtual bool UnpauseGame_Implementation() override;
+};
