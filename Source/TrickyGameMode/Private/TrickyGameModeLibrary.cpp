2638f2c9f0aee88bd56dd8a9f556cc1070d5719c
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-20 21:05:39 +0200

    Create blueprint library

---
+++ Source/TrickyGameMode/Private/TrickyGameModeLibrary.cpp
@@ -0,0 +1,168 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "TrickyGameModeLibrary.h"
+
+#include "TrickyGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+
+ATrickyGameModeBase* UTrickyGameModeLibrary::GetTrickyGameMode(const UObject* WorldContextObject)
+{
+	return Cast<ATrickyGameModeBase>(UGameplayStatics::GetGameMode(WorldContextObject));
+}
+
+bool UTrickyGameModeLibrary::StartGame(const UObject* WorldContextObject)
+{
+	if (!ImplementsGameStateInterface(WorldContextObject))
+	{
+		return false;
+	}
+
+	return IGameStateControllerInterface::Execute_StartGame(UGameplayStatics::GetGameMode(WorldContextObject));
+}
+
+bool UTrickyGameModeLibrary::StopGame(const UObject* WorldContextObject, const EGameInactivityReason Reason)
+{
+	if (!ImplementsGameStateInterface(WorldContextObject))
+	{
+		return false;
+	}
+
+	return IGameStateControllerInterface::Execute_StopGame(UGameplayStatics::GetGameMode(WorldContextObject), Reason);
+}
+
+bool UTrickyGameModeLibrary::FinishGame(const UObject* WorldContextObject, const EGameResult Result)
+{
+	if (!ImplementsGameStateInterface(WorldContextObject))
+	{
+		return false;
+	}
+
+	return IGameStateControllerInterface::Execute_FinishGame(UGameplayStatics::GetGameMode(WorldContextObject), Result);
+}
+
+bool UTrickyGameModeLibrary::ChangeInactivityReason(const UObject* WorldContextObject, EGameInactivityReason Reason)
+{
+	if (!ImplementsGameStateInterface(WorldContextObject))
+	{
+		return false;
+	}
+
+	return IGameStateControllerInterface::Execute_ChangeInactivityReason(
+		UGameplayStatics::GetGameMode(WorldContextObject), Reason);
+}
+
+bool UTrickyGameModeLibrary::StartPreparation(const UObject* WorldContextObject)
+{
+	if (!ImplementsGameStateInterface(WorldContextObject))
+	{
+		return false;
+	}
+
+	return IGameStateControllerInterface::Execute_StartPreparation(UGameplayStatics::GetGameMode(WorldContextObject));
+}
+
+bool UTrickyGameModeLibrary::StartCutscene(const UObject* WorldContextObject)
+{
+	if (!ImplementsGameStateInterface(WorldContextObject))
+	{
+		return false;
+	}
+
+	return IGameStateControllerInterface::Execute_StartCutscene(UGameplayStatics::GetGameMode(WorldContextObject));
+}
+
+bool UTrickyGameModeLibrary::StartTransition(const UObject* WorldContextObject)
+{
+	if (!ImplementsGameStateInterface(WorldContextObject))
+	{
+		return false;
+	}
+
+	return IGameStateControllerInterface::Execute_StartTransition(UGameplayStatics::GetGameMode(WorldContextObject));
+}
+
+EGameState UTrickyGameModeLibrary::GetGameState(const UObject* WorldContextObject)
+{
+	if (!ImplementsGameStateInterface(WorldContextObject))
+	{
+		return EGameState::Inactive;
+	}
+
+	return IGameStateControllerInterface::Execute_GetGameState(UGameplayStatics::GetGameMode(WorldContextObject));
+}
+
+EGameResult UTrickyGameModeLibrary::GetGameResult(const UObject* WorldContextObject)
+{
+	if (!ImplementsGameStateInterface(WorldContextObject))
+	{
+		return EGameResult::None;
+	}
+
+	return IGameStateControllerInterface::Execute_GetGameResult(UGameplayStatics::GetGameMode(WorldContextObject));
+}
+
+EGameInactivityReason UTrickyGameModeLibrary::GetInactivityReason(const UObject* WorldContextObject)
+{
+	if (!ImplementsGameStateInterface(WorldContextObject))
+	{
+		return EGameInactivityReason::None;
+	}
+
+	return IGameStateControllerInterface::Execute_GetGameInactivityReason(
+		UGameplayStatics::GetGameMode(WorldContextObject));
+}
+
+float UTrickyGameModeLibrary::GetGameElapsedTime(const UObject* WorldContextObject)
+{
+	if (!ImplementsGameStateInterface(WorldContextObject))
+	{
+		return -1.f;
+	}
+
+	return IGameStateControllerInterface::Execute_GetGameElapsedTime(UGameplayStatics::GetGameMode(WorldContextObject));
+}
+
+float UTrickyGameModeLibrary::GetGameRemainingTime(const UObject* WorldContextObject)
+{
+	if (!ImplementsGameStateInterface(WorldContextObject))
+	{
+		return -1.f;
+	}
+
+	return IGameStateControllerInterface::Execute_GetGameRemainingTime(
+		UGameplayStatics::GetGameMode(WorldContextObject));
+}
+
+float UTrickyGameModeLibrary::GetGamePreparationRemainingTime(const UObject* WorldContextObject)
+{
+	const ATrickyGameModeBase* GameMode = GetTrickyGameMode(WorldContextObject);
+	const UWorld* World = GameMode->GetWorld();
+
+	if (!IsValid(GameMode) || !IsValid(World))
+	{
+		return -1.f;
+	}
+
+	return World->GetTimerManager().GetTimerRemaining(GameMode->GetPreparationTimerHandle());
+}
+
+float UTrickyGameModeLibrary::GetGamePreparationElapsedTime(const UObject* WorldContextObject)
+{
+	const ATrickyGameModeBase* GameMode = GetTrickyGameMode(WorldContextObject);
+	const UWorld* World = GameMode->GetWorld();
+
+	if (!IsValid(GameMode) || !IsValid(World))
+	{
+		return -1.f;
+	}
+
+	return World->GetTimerManager().GetTimerElapsed(GameMode->GetPreparationTimerHandle());
+}
+
+bool UTrickyGameModeLibrary::ImplementsGameStateInterface(const UObject* WorldContextObject)
+{
+	const AGameModeBase* GameMode = UGameplayStatics::GetGameMode(WorldContextObject);
+
+	return GameMode->Implements<IGameStateControllerInterface>();
+}
---
+++ Source/TrickyGameMode/Public/TrickyGameModeLibrary.h
@@ -0,0 +1,73 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TrickyGameModeLibrary.generated.h"
+
+enum class EGameState : uint8;
+enum class EGameResult : uint8;
+enum class EGameInactivityReason : uint8;
+class ATrickyGameModeBase;
+/**
+ * 
+ */
+UCLASS()
+class TRICKYGAMEMODE_API UTrickyGameModeLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static ATrickyGameModeBase* GetTrickyGameMode(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static bool StartGame(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static bool StopGame(const UObject* WorldContextObject, EGameInactivityReason Reason);
+
+	UFUNCTION(BlueprintCallable, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static bool FinishGame(const UObject* WorldContextObject, EGameResult Result);
+
+	UFUNCTION(BlueprintCallable, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static bool ChangeInactivityReason(const UObject* WorldContextObject, EGameInactivityReason Reason);
+
+	UFUNCTION(BlueprintCallable, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static bool StartPreparation(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static bool StartCutscene(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static bool StartTransition(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintPure, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static EGameState GetGameState(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintPure, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static EGameResult GetGameResult(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintPure, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static EGameInactivityReason GetInactivityReason(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintPure, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static float GetGameElapsedTime(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintPure, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static float GetGameRemainingTime(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintPure, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static float GetGamePreparationRemainingTime(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintPure, Category=TrickyGameMode, meta=(WorldContext="WorldContextObject"))
+	static float GetGamePreparationElapsedTime(const UObject* WorldContextObject);
+	
+private:
+	UFUNCTION()
+	static bool ImplementsGameStateInterface(const UObject* WorldContextObject);
+};
+
+
+
